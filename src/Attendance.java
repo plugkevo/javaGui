32e308305a20d6ed3b10bbf2518e667c3835261e
plugkevo <munenekevin366@gmail.com>
2024-11-02 11:29:25 +0300

    design done

--- src/Attendance.java
+++ src/Attendance.java
@@ -6,29 +6,39 @@ public class Attendance extends JFrame {
 
     private JPanel panel1;
     private JLabel attendanceManagementSystemTextArea;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
+    private JLabel AdminPanelbtn;
+    private JButton dashboardButton;
+    private JButton teacherButton;
+    private JButton studentButton;
+    private JButton batchButton;
+    private JButton reportButton;
+    private JButton exitButton;
     private JComboBox comboBox1;
     private JComboBox comboBox2;
     private JComboBox comboBox3;
     private JTextField textField1;
     private JTextField textField2;
+    private JButton batchattendance;
+    private JButton batchandstudentatt;
+    private JButton allbatches;
+    private JButton batchattwdate;
+
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("UBPrivacy");
+
+
+
+        JFrame frame = new JFrame("ATTENDANCE FORM");
         frame.setContentPane(new Attendance().panel1);
         frame.setSize(300,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
+
+
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+
 }
